75c85727b39e9c4678bd26d379221516973d5bbb
Anonymous Applicant <anonymous@example.com>
2018-05-12 23:51:57 -0700

    long way to getting a downcased hash

--- src/my_exercise/core.clj
+++ src/my_exercise/core.clj
@@ -1,7 +1,7 @@
 (ns my-exercise.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [clj-http.client :as client]
+            [my-exercise.results :refer [parse-form-params]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.reload :refer [wrap-reload]]
             [my-exercise.home :as home]))
@@ -11,7 +11,7 @@
   ; https://gist.github.com/zehnpaard/2071c3f55ed319aa8528d54d90f557e3#file-simple-hiccup-routes-clj-L13
   ; http://clojure-doc.org/articles/tutorials/basic_web_development.html#set-up-your-routes
   ; https://stackoverflow.com/a/32284706
-  (POST "/search" [& params] (str "<h1>Hello " params "</h1>"))
+  (POST "/search" [& params] (str "<h1>Hello " (parse-form-params params) "</h1>"))
   (route/resources "/")
   (route/not-found "Not found"))
 
--- src/my_exercise/results.clj
+++ src/my_exercise/results.clj
@@ -1,3 +1,17 @@
 (ns my-exercise.results
-  (:require [hiccup.page :refer [html5]]))
+  (:require [hiccup.page :refer [html5]]
+            [clj-http.client :as client]))
 
+(def base-uri
+  "https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:ca")
+
+(defn parse-form-params [params]
+  (let [{:keys [street street-2 state city zip]} params
+        m {:street   street
+           :street-2 street-2
+           :state    state
+           :city     city
+           :zip      zip}]
+    (->> (remove (fn [[_ v]] (empty? v)) m)
+         (map (fn [[k v]] [k (clojure.string/lower-case v)]))
+         (into {}))))
