ce76140a455abadef09260509041f4ae781b3bd0
Anonymous Applicant <anonymous@example.com>
2018-05-13 00:17:16 -0700

    handle state and place

--- src/my_exercise/results.clj
+++ src/my_exercise/results.clj
@@ -22,9 +22,23 @@
          (map (fn [[k v]] [k (clojure.string/replace v #"\s+" "_")]))
          (into {}))))
 
+(defn compose-uri
+  ([state]
+   (str base-uri (state-ocd-id state)))
+  ([state place]
+   (str base-uri (state-ocd-id state) "," (state-ocd-id state) "/place:" place)))
+
 (defn make-the-call [params]
-  (let [state (:state (parse-form-params params))]
-    (read-string (:body (client/get (str base-uri (state-ocd-id state)))))))
+  (let [{:keys [state city]} params]
+    (if city
+      (read-string (:body (client/get (compose-uri state city))))
+      (read-string (:body (client/get (compose-uri state)))))))
+
+;(defn page [request]
+;  (html5
+;    (header request)
+;    (instructions request)
+;    (address-form request)))
 
 ; how to read a call
 ; (read-string (:body (client/get (str base-uri (state-ocd-id "ca")))))
